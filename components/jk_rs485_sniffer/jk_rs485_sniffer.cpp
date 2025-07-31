f18fb364e726fae97f2531792feb64a74dced6d2
rabbit3dcustom <rabbit3dcustom@gmail.com>
2025-07-31 12:50:30 +0200

    dd

--- components/jk_rs485_sniffer/jk_rs485_sniffer.cpp
+++ components/jk_rs485_sniffer/jk_rs485_sniffer.cpp
@@ -640,8 +640,11 @@ void JkRS485Sniffer::loop() {
       {
         ESP_LOGE(TAG, "JkRS485Sniffer::manage_rx_buffer_()-JKPB_RS485_RESPONSE_SIZE 2: CHECKSUM OK 0x%02X == 0x%02X", computed_checksum, remote_checksum);
         uint8_t address = 0;
+        uint8_t frame_type = 0;
         address = raw_ptr[JKPB_RS485_ADDRESS_OF_RS485_ADDRESS];
-        ESP_LOGE(TAG, "JkRS485Sniffer::manage_rx_buffer_()-JKPB_RS485_RESPONSE_SIZE 2: address: 0x%02X", address);
+        frame_type = raw_ptr[JKPB_RS485_FRAME_TYPE_ADDRESS]
+
+        ESP_LOGE(TAG, "JkRS485Sniffer::manage_rx_buffer_()-JKPB_RS485_RESPONSE_SIZE 2: address: 0x%02X - frame: 0x%02X ", address, frame_type);
 
       }
 
