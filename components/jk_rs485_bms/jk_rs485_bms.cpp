68c1e986e8442d1522e31cd4d61357c74a23d117
rabbit3dcustom <rabbit3dcustom@gmail.com>
2025-08-17 20:35:54 +0200

    pri

--- components/jk_rs485_bms/jk_rs485_bms.cpp
+++ components/jk_rs485_bms/jk_rs485_bms.cpp
@@ -793,8 +793,9 @@ void JkRS485Bms::on_jk_rs485_sniffer_data(const uint8_t &origin_address, const u
         ESP_LOGVV(TAG, "frame_type: 0x03 ==========================================================================");
         ESP_LOGVV(TAG, "frame_type: 0x03 =============================START 0x03===================================");
         ESP_LOGVV(TAG, "frame_type: 0x03 ==========================================================================");
+        ESP_LOGD(TAG, "  %s", format_hex_pretty(&data.front(), 150).c_str());
 
-         print_data_segmented(data);
+        print_data_segmented(data);
         
         this->decode_device_info_(data);
 
