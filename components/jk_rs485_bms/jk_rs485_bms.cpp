be38a28c0e7b9fdbe07618c8ffb2e41562385577
rabbit3dcustom <rabbit3dcustom@gmail.com>
2025-08-18 10:35:34 +0200

    commented device_info

--- components/jk_rs485_bms/jk_rs485_bms.cpp
+++ components/jk_rs485_bms/jk_rs485_bms.cpp
@@ -1812,20 +1812,15 @@ void JkRS485Bms::decode_device_info_(const std::vector<uint8_t> &data) {
   ESP_LOGVV(TAG, "  %s", format_hex_pretty(&data.front(), 160).c_str());
   ESP_LOGVV(TAG, "  %s", format_hex_pretty(&data.front() + 160, data.size() - 160).c_str());
 
+  /*
   this->publish_state_(this->info_vendorid_text_sensor_, std::string(data.begin() + 6, data.begin() + 6 + 16).c_str());
-  this->publish_state_(this->info_hardware_version_text_sensor_,
-                       std::string(data.begin() + 22, data.begin() + 22 + 8).c_str());
-  this->publish_state_(this->info_software_version_text_sensor_,
-                       std::string(data.begin() + 30, data.begin() + 30 + 8).c_str());
-  this->publish_state_(this->info_device_name_text_sensor_,
-                       std::string(data.begin() + 46, data.begin() + 46 + 16).c_str());
-  this->publish_state_(this->info_device_password_text_sensor_,
-                       std::string(data.begin() + 62, data.begin() + 62 + 16).c_str());
-  this->publish_state_(this->info_device_serial_number_text_sensor_,
-                       std::string(data.begin() + 86, data.begin() + 86 + 11).c_str());
-  this->publish_state_(this->info_device_setup_passcode_text_sensor_,
-                       std::string(data.begin() + 118, data.begin() + 118 + 16).c_str());
-
+  this->publish_state_(this->info_hardware_version_text_sensor_, std::string(data.begin() + 22, data.begin() + 22 + 8).c_str());
+  this->publish_state_(this->info_software_version_text_sensor_, std::string(data.begin() + 30, data.begin() + 30 + 8).c_str());
+  this->publish_state_(this->info_device_name_text_sensor_, std::string(data.begin() + 46, data.begin() + 46 + 16).c_str());
+  this->publish_state_(this->info_device_password_text_sensor_, std::string(data.begin() + 62, data.begin() + 62 + 16).c_str());
+  this->publish_state_(this->info_device_serial_number_text_sensor_, std::string(data.begin() + 86, data.begin() + 86 + 11).c_str());
+  this->publish_state_(this->info_device_setup_passcode_text_sensor_, std::string(data.begin() + 118, data.begin() + 118 + 16).c_str());
+*/
   this->publish_state_(this->uart1_protocol_number_sensor_, (uint8_t) data[178]);
   this->publish_state_(this->uart2_protocol_number_sensor_, (uint8_t) data[212]);
 
